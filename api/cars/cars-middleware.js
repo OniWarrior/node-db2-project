e0a1e3c936f2ad8445c5531168b0cedc0588f876
Stephen Aranda <aranda.stephen88@gmail.com>
2021-06-19 20:41:25 -0500

    checkCarPayload function in cars-middleware is completed

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -10,7 +10,7 @@ const checkCarId = (req, res, next) => {
       req.car = car
       next()
     }
-    else{
+    else{   // if not return 404 status indicating car with specified id not found
       next({
         status:404,
         message:`Car with id ${req.params.id} is not found`
@@ -26,6 +26,44 @@ const checkCarId = (req, res, next) => {
 
 const checkCarPayload = (req, res, next) => {
   // DO YOUR MAGIC
+  // Destructure payload from req.body
+  const {
+    vin,
+    make,
+    model,
+    mileage    
+  } = req.body
+
+  // if vin,make,model,or mileage is missing,respond with error message
+  if(!vin){
+    next({
+      status:400,
+      message:'vin field is missing'
+    })    
+  }
+  else if(!make){
+    next({
+      status:400,
+      message:'Make field is missing'
+    })         
+  }
+  else if(!model){
+    next({
+      status:400,
+      message:'Model field is missing'
+    })
+  }
+  else if(!mileage){
+    next({
+      status:400,
+      message:'Mileage field is missing'
+    })
+  }
+  else{
+    next()
+  }
+ 
+
 }
 
 const checkVinNumberValid = (req, res, next) => {
